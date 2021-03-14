1af740e919a341a8612676f8907f2dfafc2d24ef
Maria Rehman <79382936+maronomics@users.noreply.github.com>
2021-03-14 19:00:43 +0500

    Comments added

---
+++ DataStructures/Stack/Stack.js
@@ -0,0 +1,11 @@
+/* Stack!
+ * 
+ * An element is added on top of the Stack and Only the top element is deleted.
+ * Implemented Stack using Array, only at the end element is added and removed.
+ *
+ *
+ */
+
+// Functions:
+// push, poop, peek, view, length
+
