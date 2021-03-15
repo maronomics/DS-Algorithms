3b90f194cabb3a9cb2f0fc1f88b1eb2fd3e48dec
Maria Rehman <79382936+maronomics@users.noreply.github.com>
2021-03-15 19:58:33 +0500

    Fibonacci Definition added

---
+++ Recursive/Fibonacci/Fibonacci.js
@@ -0,0 +1,12 @@
+/* 
+* Fibonacci Sequence using Recursion
+*
+* Fibonacci sequence, 'the golden ratio' or ' The Fibonacci Spiral' 
+* is a sequence of numbers where each number is the sum of the two preceding ones,
+* starting from 0 and 1. (via wikipedia)
+* There are various methods to get nth Fibonacci numbers. This example uses Recursion.
+* 
+* Implementation: Given a number n, print n-th Fibonacci Number.
+*
+*/
+
