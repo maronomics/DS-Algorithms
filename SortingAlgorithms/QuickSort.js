9a3c3c19b8c1560b4519a7d7c0b3942bdd13481e
Maria Rehman <79382936+maronomics@users.noreply.github.com>
2021-03-16 19:19:56 +0500

    QuickSort.js Created

---
+++ SortingAlgorithms/QuickSort.js
@@ -0,0 +1,16 @@
+/* Quick Sort
+ * Uses a divide and conquer strategy.
+ * It picks an element as pivot and partitions the given array around the picked pivot
+ * (GeeksforGeeks)
+ *
+ */
+
+function quickSort (items){
+    var length = items.length
+    if (length <= 1){
+        return items
+    }
+
+    var PIVOT = item[0]
+    
+}
