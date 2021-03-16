5c72de7abb56aeb55fb87c9b120168d0d9b00b4d
Maria Rehman <79382936+maronomics@users.noreply.github.com>
2021-03-16 19:52:42 +0500

    Fixed error 'undefined'

--- SortingAlgorithms/InsertionSort.js
+++ SortingAlgorithms/InsertionSort.js
@@ -5,3 +5,32 @@
  * (GeeksforGeeks)
  * 
  */
+
+function insertSort(unsortedList){
+    var length = unsortedList.length
+
+    for (var i = 1; i < length; i++){
+        // Copy current element
+        var temp = unsortedList[i]
+        // Check through the sorted part and comparew ith temp
+        // If Greater then push element
+        for (var j = i - 1; j >= 0 && (unsortedList[j] > temp); j--){
+            // Shift item
+            unsortedList[j + 1] = unsortedList[j]
+        }
+
+        // insert the copied number at the correct position in sorted list
+        unsortedList[j + 1] = temp
+    }
+    return unsortedList
+}
+
+
+// Implementation
+
+var sort = [3, 2, 11, 7, 13, 19, 17, 5, 41, 29, 23,37, 31]
+// Array before sorting
+console.log('Array before sort: ' + sort + '\n')
+// Array after sorting
+console.log('Array after sort: ' + insertSort(sort) + '\n')
+
