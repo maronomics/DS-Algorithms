b184608978467c871a2090a5166c1a6b150b5dd8
Maria Rehman <79382936+maronomics@users.noreply.github.com>
2021-03-16 21:31:40 +0500

    SelectionSort.js updated

--- SortingAlgorithms/SelectionSort.js
+++ SortingAlgorithms/SelectionSort.js
@@ -1,6 +1,9 @@
 /* Selection Sort
  * 
- * 
- *
+ * Sorts an array by repeatedly finding the minimum element from unsorted part,
+ * and putting it at the beginning.
+ * Maintains two subarrays in a given subarray, one which is already sorted and
+ * remaining subarray which is unsorted.
  *
  */
+
