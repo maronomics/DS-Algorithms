f29fed91ac5eb10e6e6126b53788bd81ef032c3e
Maria Rehman <79382936+maronomics@users.noreply.github.com>
2021-03-16 19:26:57 +0500

    QuickSort.js Updated

--- SortingAlgorithms/QuickSort.js
+++ SortingAlgorithms/QuickSort.js
@@ -12,5 +12,29 @@ function quickSort (items){
     }
 
     var PIVOT = item[0]
-    
+    var GREATER = []
+    var LESSER = []
+
+    for (var i = 1; i < length; i++){
+        if(items[i] > PIVOT){
+            GREATER.push(items[i])
+        } else {
+            LESSER.push(items[i])
+        }
+    }
+
+    var sorted = quickSort(LESSER)
+    sorted.push(PIVOT)
+    sorted = sorted.concat(quickSort(GREATER))
+
+    return sorted
 }
+
+// Implementation
+
+var sort = [1,2,11,7,9,19,17,5]
+// Array before sorting
+console.log('Array before sort: ' + sort + '\n')
+// Array after sorting
+console.log('Array after sort: ' + quickSort(sort) + '\n')
+
