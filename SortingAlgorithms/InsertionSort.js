ef0ac8b1a957bbf9701bd20bb5629564e65b9833
Maria Rehman <79382936+maronomics@users.noreply.github.com>
2021-03-16 19:38:48 +0500

    Started Insertion Sort

---
+++ SortingAlgorithms/InsertionSort.js
@@ -0,0 +1,7 @@
+/* Insertion Sort
+ * Works the same way sorting playing cards work.
+ * The array is virtually split into a sorted and an unsorted part.
+ * Values from unsorted array ispicked and placed in sorted at correct position.
+ * (GeeksforGeeks)
+ * 
+ */
