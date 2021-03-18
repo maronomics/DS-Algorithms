d559cde1a9408c8a45f568107847fe872071a17a
Maria Rehman <79382936+maronomics@users.noreply.github.com>
2021-03-18 20:13:33 +0500

    bubbleSort.js updated

--- SortingAlgorithms/BubbleSort.js
+++ SortingAlgorithms/BubbleSort.js
@@ -10,7 +10,7 @@ function bubbleSort(arr){
     for (let i = 0; i < arr.length - 1; i++){
         for (let j = 0; j < arr.length - i - 1; j++){
             if(arr[j]> arr[j + 1]){
-                console.log(arr)
+                // console.log(arr)
                 var temp = arr[j] 
                 arr[j] = arr[j + 1]
                 arr[j + 1] = temp
