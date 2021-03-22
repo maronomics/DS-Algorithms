6e7eb7e21e08fe2b7afbb1188cd8e289c87d6831
Maria Rehman <79382936+maronomics@users.noreply.github.com>
2021-03-22 17:32:26 +0500

    Readme.md updated

---
+++ SortingAlgorithms/HeapSort.js
@@ -0,0 +1,7 @@
+/* Heap Sort
+ * A comparison based sorting techniques based on binary heap data structure.
+ * It is similar to selection sort where we first find the maximum element and
+ * place the maximum element at the end. The same process is repeated for all 
+ * the remaining elements
+ * 
+*/
